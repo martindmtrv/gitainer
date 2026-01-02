68741adc3669aee46eea87e2d1e8d41ab3580fcd
Martin Dimitrov <martin3dimitrov@gmail.com>
2026-01-01 20:02:10 -0800

    bump version to 0.0.2

--- packages/gitainer-extension/publish.sh
+++ packages/gitainer-extension/publish.sh
@@ -23,14 +23,25 @@ fi
 
 # Publish the extension
 # Usage: ./publish.sh [PAT]
-if [ -z "$1" ]; then
+PUBLISH_KEY=$1
+
+if [ -z "$PUBLISH_KEY" ]; then
+    if [ -f .env ]; then
+        echo "Loading PAT from .env..."
+        # Extract GITAINER_MARKETPLACE_PUBLISH_KEY from .env
+        PUBLISH_KEY=$(grep '^GITAINER_MARKETPLACE_PUBLISH_KEY=' .env | cut -d '=' -f2- | tr -d '"' | tr -d "'")
+    fi
+fi
+
+if [ -z "$PUBLISH_KEY" ]; then
     echo "Usage: ./publish.sh <Personal-Access-Token>"
+    echo "Alternatively, set GITAINER_MARKETPLACE_PUBLISH_KEY in a .env file."
     echo "You can create a PAT at https://dev.azure.com/"
     exit 1
 fi
 
 echo "Publishing extension..."
-vsce publish -p $1
+vsce publish -p $PUBLISH_KEY
 
 if [ $? -eq 0 ]; then
     echo "Extension published successfully!"
